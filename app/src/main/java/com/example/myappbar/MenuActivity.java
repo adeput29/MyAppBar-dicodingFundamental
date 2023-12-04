8a34831c6b77c1e235f80b80954a99d4f2e3142f
Ade Putra <adeputra.c@gmail.com>
2023-12-04 10:44:56 +0700

    CheckPoint()

--- app/src/main/java/com/example/myappbar/MenuActivity.java
+++ app/src/main/java/com/example/myappbar/MenuActivity.java
@@ -3,12 +3,33 @@ package com.example.myappbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+
+import com.example.myappbar.databinding.ActivityMainBinding;
+import com.example.myappbar.databinding.ActivityMenuBinding;
 
 public class MenuActivity extends AppCompatActivity {
-
+    private ActivityMenuBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        //setContentView(R.layout.activity_menu);
+        binding = ActivityMenuBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.searchBar);
+
+        binding.searchBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
